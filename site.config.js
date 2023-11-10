e5fcbe90cd58d259aa3ad01aed56a2169fdd0a89
Remco van der Meer <50025292+1ncendium@users.noreply.github.com>
2023-11-10 13:23:21 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,12 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Remco van der Meer",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Ethical hacker",
+    bio: "Not your stereotype hacker",
+    linkedin: "https://www.linkedin.com/in/remco-vandermeer/",
+    github: "https://github.com/1ncendium",
   },
   projects: [
     {
